3acb63a94dfb22f92ff32a535cfae04876f74d5e
tarunmv30 <81204028+tarunmv30@users.noreply.github.com>
2022-06-22 15:50:16 -0500

    This is a basic project that works with push notifications. Just need an APNS Key from apple account and need to sync that with he firebase in the dashboard.

--- NotificationDemoConsole/ContentView.swift
+++ NotificationDemoConsole/ContentView.swift
@@ -4,6 +4,21 @@
 //
 //  Created by Tarun Reddy on 6/22/22.
 //
+// STEPS:
+//Create a firebase project
+//configure firebase in xcode project using package manager + firebaseapp.configure
+//turn on background modes + push notifcations in the capabilities section
+//use apns push notifications key in the firebase dashboard
+//have this code in place
+//then go to the firebase console and compose a notification
+//use the token given by the device, once you plug your device in and run the app
+
+
+//You can use firebase cloud functions to do this automatically, rather than manually creating the notification using firebase triggers 
+
+
+
+
 
 import SwiftUI
 import FirebaseFirestore
